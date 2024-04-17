22032e2b0dbe8c4dfb9df1dcc8b8bf065d476df0
AmbrizAlberto <albertpoambez@gmail.com>
2024-04-17 16:08:40 -0600

    Responsive & New style for containers changed

--- app/page.jsx
+++ app/page.jsx
@@ -9,6 +9,9 @@ import Navbar from './components/navbar';
 import styles from "./css/main.css";
 import stylerespons from "./css/mainrespons.css";
 import miImagen from './images/1mb.jpeg';
+import Tecnologias from './components/tecnologies';
+import Proyectos from './components/projects';
+
 
 
 export default function Portafolio() {
@@ -96,107 +99,30 @@ export default function Portafolio() {
           </div>
         </div>
       </div>
-      <div className="lenguajes">
-        <h2>TECNOLOGIAS</h2>
 
 
-        <div class="grid-containertec">
-          <div class="grid-itemtec">
-            <h2>Frontend</h2>
-             <div className="tecnologias">
-              <div class="image-with-text">
-                <Image src="https://cdn-icons-png.flaticon.com/512/732/732212.png" width={40} height={40} alt="" />
-                <p>HTML</p>
-              </div>
-              <div class="image-with-text">
-                <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/2048px-CSS3_logo.svg.png" width={40} height={40} alt="" />
-                <p>CSS</p>
-              </div>
-              <div class="image-with-text">
-                <Image src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" width={40} height={40} alt="" />
-                <p>JavaScript</p>
-              </div>
-            </div>
-          </div>
-          <div class="grid-itemtec">
-            <h2>Backend</h2>
-            <div className="tecnologias">
-              <div class="image-with-text">
-                <Image src="https://camo.githubusercontent.com/57849c6a718458e32ed51527a073b72d8f1737ee515ab6f80d5a9107a485f40c/68747470733a2f2f7777772e6461746f636d732d6173736574732e636f6d2f37353934312f313635373730373837382d6e6578746a735f6c6f676f2e706e67" width={40} height={40} alt="" />
-                <p>NextJS</p>
-              </div>
-              <div class="image-with-text">
-                <Image src="https://cdn-icons-png.flaticon.com/512/5968/5968313.png" width={40} height={40} alt="" />
-                <p>MySQL</p>
-              </div>
-              <div class="image-with-text">
-                <Image src="https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png" width={40} height={40} alt="" />
-                <p>NodeJS</p>
-              </div>
-            </div>
-
-          </div>
-          <div class="grid-itemtec">
-            <h2>Aprendiendo</h2>
-            <div className="tecnologias">
-              <div class="image-with-text">
-                <Image src="https://cdn4.iconfinder.com/data/icons/logos-3/504/Swift-2-512.png" width={40} height={40} alt="" />
-                <p>Swift</p>
-              </div>
-              <div class="image-with-text">
-                <Image src="https://cdn.iconscout.com/icon/free/png-256/free-python-3521655-2945099.png?f=webp" width={40} height={40} alt="" />
-                <p>Python</p>
-              </div>
-            </div>
-
-          </div>
-          <div class="grid-itemtec">
-            <h2>Herramientas</h2>
-
-          </div>
-
-        </div>
+      <Tecnologias/>
       
-      </div>
-      
-      <div className='projects'>
-        <h2>PROYECTOS</h2>
-        <div className="grid-containerprj">
-          <button className="container-project">
-            <div className="nameprj">Harmony & Heaven</div>
-            <div className="categorie">Grupal</div>
-            <div className="photoprj">
-              <Image src="" alt="" width={40} height={40} />
-            </div>
-            <div className="descriptionprj">
-              <h4>Tienda de instrumentos con CRUD, Inicio de sesión y Punto de venta.</h4>
-            </div>
-            <div className="lenguajesprj">
-              <Image src="https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png" width={40} height={40} alt="" />
-              <Image src="https://static-00.iconduck.com/assets.00/database-mysql-icon-1954x2048-08uox8qu.png" width={40} height={40} alt="" />
-            </div>
-            <div className="github">
-              <i className="bi bi-github"></i>Ver Repositorio
-            </div>
+
+      <Proyectos/>
+
+      <div className="contactme">
+        <h1 className="contactmett">CONTACTAME</h1>
+        <h1>CORREO</h1>
+        <div className="correo">
+          <span id="url" className='correotxt'>albertpoambez@gmail.com</span>
+          <button className='correobtn' onClick={() => copiarAlPortapapeles('url')}>
+            {copiadoCorreo ? 'Copiado' : 'Copiar'}
           </button>
         </div>
-        <div className="contactme">
-          <h1 className="contactmett">CONTACTAME</h1>
-          <h1>CORREO</h1>
-          <div className="correo">
-            <span id="url" className='correotxt'>albertpoambez@gmail.com</span>
-            <button className='correobtn' onClick={() => copiarAlPortapapeles('url')}>
-              {copiadoCorreo ? 'Copiado' : 'Copiar'}
-            </button>
-          </div>
-          <h1>TELEFONO</h1>
-          <div className="telefono">
-            <span id="tel" className='correotxt'>+52 3141576598</span>
-            <button className='correobtn' onClick={() => copiarTelefono('tel')}>
-              {copiadoTelefono ? 'Copiado' : 'Copiar'}
-            </button>
-          </div>
+        <h1>TELEFONO</h1>
+        <div className="telefono">
+          <span id="tel" className='correotxt'>+52 3141576598</span>
+          <button className='correobtn' onClick={() => copiarTelefono('tel')}>
+            {copiadoTelefono ? 'Copiado' : 'Copiar'}
+          </button>
         </div>
+
       </div>
       <Head>
         <script src="https://platform.linkedin.com/badges/js/profile.js" async defer />
