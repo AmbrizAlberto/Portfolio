f34bab8f44db7ec623793b09b28bd86a4a560169
AmbrizAlberto <albertpoambez@gmail.com>
2024-04-19 12:12:11 -0600

    View Updated v1

--- app/components/projects.jsx
+++ app/components/projects.jsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 import { X } from 'react-feather';
 import React, { useState } from 'react';
 
-import HyH from '../images/Harmony&HeavenM.jpg';
-import HyH1 from '../images/Harmony&HeavenL.jpg';
+import HyH from '../images/HarmonyAndHeavenM.jpg';
+import HyH1 from '../images/HarmonyAndHeavenL.jpg';
 import PEE from '../images/PickEm.jpg';
 
 import "./modal.css";
@@ -35,7 +35,7 @@ const Proyectos = () => {
       <div className="grid-containerprj">
 
         <button className="container-project" onClick={() => openModalForProject("harmony_heaven")}>
-          <div className="nameprj">Harmony &amp; Heaven</div>
+          <div className="nameprj">HarmonyAndHeaven</div>
           <div className="categorie">Grupal</div>
           <div className="photoprj">
             <Image src={HyH} alt="" width={"auto"} height={"auto"} />
@@ -96,7 +96,7 @@ const Proyectos = () => {
               <X size={24} />
             </button>
 
-            <div className="nameprj1">Harmony &amp; Heaven</div>
+            <div className="nameprj1">Harmony And Heaven</div>
             <div className="categorie1">Grupal</div>
             <div className="lenguajesprj1">
               <Image src="https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png" width={40} height={40} alt="" />
@@ -110,8 +110,8 @@ const Proyectos = () => {
               <h4>Tienda de instrumentos CRUD, Inventario y Punto de venta.</h4>
             </div>
             <div className='descriptionprj2'>
-              Harmony &amp; Heaven se desarrolló como un inventario para una tienda de instrumentos donde se registrarán los productos con "unidades a la venta", "precio del producto" y "categoría", además se creó el punto de venta para el cliente donde podrá añadir al carrito cualquier producto disponible de la tienda.
-              <p>En el apartado de Vendedor, Harmony &amp; Heaven cuenta con un motor de búsqueda y filtrado de los datos para facilitar la recolección de productos, también cuenta con un gestor de pedidos al Proveedor de productos donde se registrarán cuando se soliciten y cuando se reciban para estar en stock añadiéndose automáticamente las cantidades recibidas en el inventario, así como un dashboard para ver resúmenes de ventas, empleados y demás cosas.
+              Harmony And Heaven se desarrolló como un inventario para una tienda de instrumentos donde se registrarán los productos con "unidades a la venta", "precio del producto" y "categoría", además se creó el punto de venta para el cliente donde podrá añadir al carrito cualquier producto disponible de la tienda.
+              <p>En el apartado de Vendedor, Harmony And Heaven cuenta con un motor de búsqueda y filtrado de los datos para facilitar la recolección de productos, también cuenta con un gestor de pedidos al Proveedor de productos donde se registrarán cuando se soliciten y cuando se reciban para estar en stock añadiéndose automáticamente las cantidades recibidas en el inventario, así como un dashboard para ver resúmenes de ventas, empleados y demás cosas.
               Los productos se pueden editar o eliminar de ser necesario.</p>
 
               <p>En el apartado de cliente (punto de venta), el cliente podrá únicamente ver los productos en stock y añadirlos al carrito para su compra, el precio total por los productos a comprar se mostrará siempre hasta realizar la compra.</p>
