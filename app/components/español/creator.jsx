b501fdf2f9336c4fe4781b83918b0fabd0fdb08e
AmbrizAlberto <albertpoambez@gmail.com>
2024-05-02 18:48:20 -0600

    Credits Updated

--- app/components/español/creator.jsx
+++ app/components/español/creator.jsx
@@ -3,7 +3,7 @@ return(
     <div className='creador'>
         <h4>Estilo de portafolio con derechos reservados <i className="bi bi-c-circle"></i></h4>
         <h4>Portafolio web realizado con NextJS</h4>
-        <i className="bi bi-github"></i>  /AmbrizAlberto
+        100% hecho por <i className="bi bi-github"></i>/AmbrizAlberto
     </div>
 )
 }
