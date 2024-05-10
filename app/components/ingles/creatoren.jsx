fba653daba5b58320bce0c15a5cc34bce13b1cf1
Al Ambriz - DEV <102885122+AmbrizAlberto@users.noreply.github.com>
2024-05-10 16:57:47 -0600

    Update creatoren.jsx

--- app/components/ingles/creatoren.jsx
+++ app/components/ingles/creatoren.jsx
@@ -1,7 +1,7 @@
 const CreadorEN = () => {
 return(
     <div className='creador'>
-        <h4>Copyright Portfolio Style <i className="bi bi-c-circle"></i></h4>
+        <h4>Copyright Briefcase Style <i className="bi bi-c-circle"></i></h4>
         <h4>Web Briefcase developed with NextJS</h4>
         100% by <i className="bi bi-github"></i>/AmbrizAlberto
     </div>
