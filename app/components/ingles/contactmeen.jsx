97804ac3cdad3a92668d7f87c2624482610805d4
Al Ambriz - DEV <102885122+AmbrizAlberto@users.noreply.github.com>
2024-05-11 21:00:50 -0600

    Update contactmeen.jsx

--- app/components/ingles/contactmeen.jsx
+++ app/components/ingles/contactmeen.jsx
@@ -31,13 +31,10 @@ const ContactameEN = () => {
             <h1 className="contactmett">CONTACT ME</h1>
             <h1>EMAIL</h1>
             <div className="correo">
-            <span id="url" className='correotxt'>albertpoambez@icloud.com</span>
-            <button className='correobtn' onClick={() => copiarAlPortapapeles('albertpoambez@icloud.com')}>
-                {copiadoCorreo ? 'Copiado' : 'Copiar'}
-            </button>
-            <a href="mailto:albertpoambez@icloud.com?subject=Correo%20Importante%20de%20Contacto%20Github%20&body=Hola%20Alberto,%0D%0A%0D%0AQuisiera%20ponerme%20en%20contacto%20contigo.%0D%0A%0D%0ASaludos,%0D%0ATu%20Nombre">
-                <img src="https://img.shields.io/badge/mail:  albertpoambez-%23EA4335.svg?style=for-the-badge&logo=icloud&logoColor=white" t=mail style="margin-bottom: 5px;" />
-            </a>
+                <span id="url" className='correotxt'>albertpoambez@icloud.com</span>
+                    <button className='correobtn' onClick={() => copiarAlPortapapeles('albertpoambez@icloud.com')}>
+                        {copiadoCorreo ? 'Copiado' : 'Copiar'}
+                    </button>
             </div>
             <h1>PHONE NUMBER</h1>
             <div className="telefono">
@@ -47,6 +44,10 @@ const ContactameEN = () => {
             </button>
             </div>
         </div>
+
+        <a href="mailto:albertpoambez@icloud.com?subject=Correo%20Importante%20de%20Contacto%20Github%20&body=Hola%20Alberto,%0D%0A%0D%0AQuisiera%20ponerme%20en%20contacto%20contigo.%0D%0A%0D%0ASaludos,%0D%0ATu%20Nombre">
+        <img src="https://img.shields.io/badge/mail:  albertpoambez-%23EA4335.svg?style=for-the-badge&logo=icloud&logoColor=white" t=mail style="margin-bottom: 5px;" />
+        </a>
         )
     }
 
