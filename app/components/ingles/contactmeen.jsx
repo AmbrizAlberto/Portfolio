31d91c225400c9d74177b7fce3abf924192691c8
Al Ambriz - DEV <102885122+AmbrizAlberto@users.noreply.github.com>
2024-05-11 20:57:02 -0600

    Update contactmeen.jsx

--- app/components/ingles/contactmeen.jsx
+++ app/components/ingles/contactmeen.jsx
@@ -35,6 +35,9 @@ const ContactameEN = () => {
             <button className='correobtn' onClick={() => copiarAlPortapapeles('albertpoambez@icloud.com')}>
                 {copiadoCorreo ? 'Copiado' : 'Copiar'}
             </button>
+            <a href="mailto:albertpoambez@icloud.com?subject=Correo%20Importante%20de%20Contacto%20Github%20&body=Hola%20Alberto,%0D%0A%0D%0AQuisiera%20ponerme%20en%20contacto%20contigo.%0D%0A%0D%0ASaludos,%0D%0ATu%20Nombre">
+                <img src="https://img.shields.io/badge/mail:  albertpoambez-%23EA4335.svg?style=for-the-badge&logo=icloud&logoColor=white" t=mail style="margin-bottom: 5px;" />
+            </a>
             </div>
             <h1>PHONE NUMBER</h1>
             <div className="telefono">
