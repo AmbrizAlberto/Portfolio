ea94b3b65232bc8ae6018a0a6121ea70cd2d9cb5
AmbrizAlberto <albertpoambez@gmail.com>
2024-05-17 13:14:27 -0600

    Photo Hover & Colors Updated

--- app/components/español/aboutme.jsx
+++ app/components/español/aboutme.jsx
@@ -1,15 +1,19 @@
+'use client'
+import { useState } from 'react';
 import Image from 'next/image';
 import miImagen from '../../images/1mb.jpeg';
+import miImagen2 from '../../images/1mb2.jpeg';
 
 const Aboutme = () => {
 
     const downloadCV = "./CVES.pdf";
+    const [hovered, setHovered] = useState(false);
 
     return (
         <div className="personaldata">
             <div className="datos1">
                 <div className="photomia">
-                    <a href={downloadCVEN} download="CV ENG - Alberto Ambriz.pdf">
+                    <a href={downloadCV} download="CV ENG - Alberto Ambriz.pdf">
                         <Image onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} src={hovered ? miImagen : miImagen2} width={"auto"} height={"auto"} priority={true} alt="" />
                     </a>
                 </div>
