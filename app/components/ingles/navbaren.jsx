7679a62c51cb99e4a081b383b1188503ac650992
AmbrizAlberto <albertpoambez@gmail.com>
2024-06-01 08:45:04 -0700

    Navbar Routes Fixed

--- app/components/ingles/navbaren.jsx
+++ app/components/ingles/navbaren.jsx
@@ -22,7 +22,7 @@ export default function NavbarEN() {
             <div className="access">
                 <a className="link" href="#"><i className="bi bi-house-fill"></i><span>Home</span></a>
                 <a className="link" href="./PersonalEN"><i className="bi bi-person-raised-hand"></i><span>Solo</span></a>
-                <a className="link" href="/"><i className="bi bi-people-fill"></i><span>In Group</span></a>
+                <a className="link" href="#"><i className="bi bi-people-fill"></i><span>In Group</span></a>
              </div>
             
         </div>
