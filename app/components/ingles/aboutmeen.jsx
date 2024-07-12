ec93d3fbf14afbc12e0965ce6801630941d123f3
Al Dev <albertpoambez@gmail.com>
2024-07-11 19:39:37 -0600

    Update aboutmeen.jsx

--- app/components/ingles/aboutmeen.jsx
+++ app/components/ingles/aboutmeen.jsx
@@ -28,7 +28,7 @@ const AboutmeEN = () => {
                 <div className="aboutme">
                     <h2>ABOUT ME...</h2>
                     <div className='descriptionme'>
-                        Software Engineering student, I am passionate about developing applications (web/mobile), their design is the most entertaining for me, I am ready to work immediately.
+                        Hello! I am Alberto Ambriz, a FullStack developer, Software Engineering student with more than 4 years of experience in the field of programming and web development. I look forward to the opportunity to discuss how I can add value to his team and meet his development goals!
                     </div>
                 </div>
             </div>
